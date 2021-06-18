818136b0532ba1845267567423da95d554afccd5
Gabriel Amaya <gabbriel.amaya@gmail.com>
2021-06-18 21:01:19 +0000

    changes in navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,28 +1,31 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <>
-            <nav className="navbar navbar-expand-lg">
-                <div className="navbar1">STARWARS</div>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
+                <Link className="navbar-brand ms-3" to="/">STARWARS</Link>
+                <button class="navbar-toggler" type="button"
+                    data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
+                    <ul className="navbar-nav mb-2 mb-lg-0 d-flex align-items-center">
                         <li className="nav-item">
                             <Link className="nav-link" to="/">Home</Link>
                         </li>
                         <li className="nav-item">
-                         <Link className="nav-link" to="/characters">Characters</Link>
-                         </li>
-                         <li className="nav-item">
-                         <Link className="nav-link" to="/planets">Planets</Link>
-                         </li>
-                         <li className="nav-item">
-                         <Link className="nav-link" to="/spaceships">Spaceships</Link>&nbsp;&nbsp;
-                         </li>
+                            <Link className="nav-link" to="/characters">Characters</Link>
+                        </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="favorites">My favourites</Link>
+                            <Link className="nav-link" to="/planets">Planets</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/spaceships">Spaceships</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/favorites">My Favorites</Link>
                         </li>
                     </ul>
                 </div>
