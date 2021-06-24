2e9a7a50299a437fa6cf780bd464c1235c2e16e9
zeroSkill <gabbriel.amaya@gmail.com>
2021-06-24 17:48:23 -0400

    showing properties on characterdetails

--- src/views/details/characterdetails.js
+++ src/views/details/characterdetails.js
@@ -10,17 +10,36 @@ function CharacterDetails() {
     useEffect(() => {
         actions.getCharacter(`http://www.swapi.tech/api/people/${params.id}`);
     }, [])
+    const getImgName = name => {
+        return name.toLowerCase().split(" ").join("-") + ".jpg";
+    };
     return (
         <>
             {!!character &&
                 character.data.result.properties.name.split(" ").join("").toLowerCase() === params.name ?
                 (
-                    <div>list of properties
-                        <ul>
-                            <li>hair color: {character.data.result.properties.hair_color}</li>
-                        </ul>
+                    <div className="card my-3 shadow">
+                        <img
+                            src={`/img/characters/${getImgName(character.data.result.properties.name)}`}
+                            className="card-img" alt="img"
+                        />
+                        <h4 className="card-title text-center">
+                            {character.data.result.properties.name}
+                        </h4>
+                        <div className="card-body">
+                            <ul className="list-group text-center ">
+                                <li className="list-group-item d-flex justify-content-between"><span>Gender:</span> <span>{character.data.result.properties.gender}</span></li>
+                                <li className="list-group-item d-flex justify-content-between"><span>Height:</span> <span>{character.data.result.properties.height}</span></li>
+                                <li className="list-group-item d-flex justify-content-between"><span>Mass:</span> <span>{character.data.result.properties.mass}</span></li>
+                                <li className="list-group-item d-flex justify-content-between"><span>Hair color:</span> <span>{character.data.result.properties.hair_color}</span></li>
+                                <li className="list-group-item d-flex justify-content-between"><span>Skin color:</span> <span>{character.data.result.properties.skin_color}</span></li>
+                                <li className="list-group-item d-flex justify-content-between"><span>Eye color:</span> <span>{character.data.result.properties.eye_color}</span></li>
+                                <li className="list-group-item d-flex justify-content-between"><span>Birth year:</span> <span>{character.data.result.properties.birth_year}</span></li>
+                                <li className="list-group-item d-flex justify-content-between"><span>Homeworld:</span> <span>{character.data.result.properties.homeworld}</span></li>
+                            </ul>
+                        </div>
                     </div>
-                ):
+                ) :
                 <LoadingSpinner />
             }
         </>
