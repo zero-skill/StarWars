d821719183471f5186bbb605db377f8ef8ab5468
zeroSkill <gabbriel.amaya@gmail.com>
2021-06-25 11:33:03 -0400

    added carousel of homepage

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,10 +1,28 @@
-import  {useContext, useState} from 'react'; 
-import {Context} from '../store/appContext';
+import { useContext, useState } from 'react';
+import { Context } from '../store/appContext';
 export const Homepage = () => {
-  
+
     return (
-        <div className="container">
-            HOLA HOME
-        </div>
+            <div id="carouselExampleFade" className="carousel slide carousel-fade my-4 mx-auto" data-bs-ride="carousel">
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src="/img/hero1.jpg" className="d-block w-100" alt="hero1" />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="/img/hero2.jpg" className="d-block w-100" alt="hero2" />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="/img/hero3.jpg" className="d-block w-100" alt="hero3" />
+                    </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
     );
 }
